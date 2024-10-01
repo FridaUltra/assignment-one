740d766bda8feac1b2cae3180b60640f82e9c43c
FridaUltra <5carlsonfrida@gmail.com>
2024-10-01 08:37:06 +0200

    ...

---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,17 @@
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { Button, Text, View } from 'react-native';
+import { RootStackParamList } from '../App';
+
+type Props = NativeStackScreenProps<RootStackParamList, 'Home'>;
+
+export default function HomeScreen({ navigation }: Props) {
+	return (
+		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+			<Text>Home Screen</Text>
+			<Button
+				title="Klicka här för detaljer"
+				onPress={() => navigation.navigate('Details')}
+			/>
+		</View>
+	);
+}
