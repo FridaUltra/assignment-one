877955a4d0ed901634b559689f58709ae1287dd9
FridaUltra <5carlsonfrida@gmail.com>
2024-10-04 19:46:07 +0200

    Apply lineargradient as background and render different colors based on weather conditions.

--- screens/WeatherScreen.tsx
+++ screens/WeatherScreen.tsx
@@ -1,5 +1,6 @@
 import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 import { Image } from 'expo-image';
+import { LinearGradient } from 'expo-linear-gradient';
 import * as Location from 'expo-location';
 import { LocationObject } from 'expo-location';
 import React, { useEffect, useState } from 'react';
@@ -51,13 +52,40 @@ export default function WeatherScreen() {
 	const feelsLikeTemp = weather?.current.feelslike_c ?? 0;
 	const windSpeed_mps = (weather?.current.wind_kph ?? 0) / 3.6;
 	const cloudiness = weather?.current.cloud ?? 0;
-	const weatherIconUrl =
-		weather?.current.condition.icon ?? require('../assets/adaptive-icon.png');
 	const weatherDescription = weather?.current.condition.text ?? 'Unknown text';
+
+	const getGradientColors = () => {
+		if (!weather) return ['#4c669f', '#3b5998', '#192f6a'];
+
+		// Choose colors base on weather conditons.
+		if (weather.current.is_day) return ['#4c669f', '#3b5998', '#192f6a'];
+		const weatherCondition = weather.current.condition.code;
+
+		switch (weatherCondition) {
+			case 1000:
+				return ['#FFD700', '#FF8C00']; // sunny
+			case 1006:
+				return ['#D3D3D3', '#A9A9A9']; // cloudy
+			case 1240:
+				return ['#000046', '#1CB5E0']; // rainy
+			case 1225:
+				return ['#E0EAF6', '#A1C4FD']; // snowy
+			case 1276:
+				return ['#20002c', '#cbb4d4']; // thunder
+			default:
+				return ['#4c669f', '#3b5998', '#192f6a']; // standard
+		}
+	};
+
+	const backgroundGradient = getGradientColors();
 
 	return (
 		<>
 			<View style={s.container}>
+				<LinearGradient
+					colors={getGradientColors()}
+					style={s.background}
+				/>
 				<View
 					style={{
 						flexDirection: 'row',
@@ -109,25 +137,25 @@ export default function WeatherScreen() {
 							},
 						],
 					}}
-					width={Dimensions.get('window').width - 20} // bredden på diagrammet
-					height={350} // höjden på diagrammet
+					width={Dimensions.get('window').width - 20}
+					height={350}
 					chartConfig={{
 						backgroundColor: '#e76f51',
-						backgroundGradientFrom: '#e29578',
-						backgroundGradientTo: '#e29578',
-						decimalPlaces: 0, // Antal decimaler
-						color: (opacity = 1) => `rgba(051, 042, 041, ${opacity})`,
-						labelColor: (opacity = 1) => `rgba(051, 042, 041, ${opacity})`,
+						backgroundGradientFrom: backgroundGradient[1],
+						backgroundGradientTo: backgroundGradient[0],
+						decimalPlaces: 0,
+						color: (opacity = 1) => `rgba(246, 253, 255, ${opacity})`,
+						labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
 						style: {
 							borderRadius: 16,
 						},
 						propsForDots: {
-							r: '9',
+							r: '7',
 							strokeWidth: '2',
-							stroke: '#ffa726',
+							stroke: '#D4D1C1',
 						},
 					}}
-					bezier // För att göra linjerna mjuka
+					bezier
 					style={{
 						marginVertical: 8,
 						borderRadius: 16,
@@ -154,5 +182,12 @@ const s = StyleSheet.create({
 		fontWeight: 'bold',
 		textAlign: 'center',
 		marginBottom: 10,
+	},
+	background: {
+		position: 'absolute',
+		left: 0,
+		right: 0,
+		top: 0,
+		height: '100%',
 	},
 });
