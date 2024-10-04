77d37fcf34d2cd3ab0a4bb3b0f04ff9dfa57bc76
FridaUltra <5carlsonfrida@gmail.com>
2024-10-04 18:31:32 +0200

    Add weather icon and description

--- screens/WeatherScreen.tsx
+++ screens/WeatherScreen.tsx
@@ -1,7 +1,8 @@
 import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import { Image } from 'expo-image';
 import * as Location from 'expo-location';
 import { LocationObject } from 'expo-location';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Dimensions, StyleSheet, Text, View } from 'react-native';
 import { LineChart } from 'react-native-chart-kit';
 import { Weather } from '../data';
@@ -10,14 +11,13 @@ const API_KEY = 'a3b13214b49043318a485725240210';
 
 export default function WeatherScreen() {
 	const [location, setLocation] = useState<LocationObject>();
-	const [errorMsg, setErrorMsg] = useState<string>();
 	const [weather, setWeather] = useState<Weather>();
 
 	useEffect(() => {
 		(async () => {
 			let { status } = await Location.requestForegroundPermissionsAsync();
 			if (status !== 'granted') {
-				setErrorMsg('Permission to access location was denied');
+				console.log('Permission to access location was denied');
 				return;
 			}
 
@@ -40,38 +40,60 @@ export default function WeatherScreen() {
 			const data: Weather = await response.json();
 			console.log(data);
 			setWeather(data);
-			console.log(`Land: ${weather!.location.country}`);
-			console.log(`namn: ${weather!.location.name}`);
+			console.log(`Land: ${weather?.location.country}`);
+			console.log(`namn: ${weather?.location.name}`);
 		}
 		fetcher();
 	}, [location]);
-
-	let text = 'Waiting..';
-	if (errorMsg) {
-		text = errorMsg;
-	} else if (location) {
-		text = JSON.stringify(location);
-	}
 
 	const locationName = weather?.location.name ?? 'Unknown Location';
 	const temp = weather?.current.temp_c ?? 0;
 	const feelsLikeTemp = weather?.current.feelslike_c ?? 0;
 	const windSpeed_mps = (weather?.current.wind_kph ?? 0) / 3.6;
 	const cloudiness = weather?.current.cloud ?? 0;
-	const weatherIconUrl = weather?.current.condition.icon ?? 'Unknown Url';
+	const weatherIconUrl =
+		weather?.current.condition.icon ?? require('../assets/adaptive-icon.png');
 	const weatherDescription = weather?.current.condition.text ?? 'Unknown text';
 
 	return (
 		<>
 			<View style={s.container}>
-				<View style={{ flexDirection: 'row', gap: 16 }}>
+				<View
+					style={{
+						flexDirection: 'row',
+						gap: 16,
+						alignItems: 'center',
+						width: '100%',
+						justifyContent: 'center',
+					}}>
 					<FontAwesome6
 						name="location-dot"
-						size={24}
+						size={36}
 						color="black"
 					/>
-					<Text style={s.title}>{locationName} - Väderdata</Text>
+					<Text style={[s.title, { fontSize: 36 }]}>{locationName}</Text>
 				</View>
+				<View
+					style={{
+						height: 100,
+						width: 100,
+					}}>
+					<Image
+						style={{ flex: 1, width: '100%' }}
+						source={
+							weather?.current.condition.icon
+								? { uri: `https:${weather?.current.condition.icon}` }
+								: null
+						}
+						placeholder={require('../assets/adaptive-icon.png')}
+						contentFit="cover"
+						transition={1000}
+					/>
+				</View>
+
+				<Text style={s.title}>{weatherDescription}</Text>
+
+				<Text style={[s.title, { paddingTop: 20 }]}>Väderdata</Text>
 
 				<LineChart
 					data={{
