ac511f538aac61cfc3567ff40c742a486fc92642
FridaUltra <5carlsonfrida@gmail.com>
2024-10-04 17:37:51 +0200

    Use location and fetch weather again in Home.  Install expo battery. and implement

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,6 +1,9 @@
 import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 import { BottomTabScreenProps } from '@react-navigation/bottom-tabs/lib/typescript/src/types';
-import { useContext } from 'react';
+import { useBatteryLevel } from 'expo-battery';
+import * as Location from 'expo-location';
+import { LocationObject } from 'expo-location';
+import { useEffect, useState } from 'react';
 import {
 	ActivityIndicator,
 	ScrollView,
@@ -9,32 +12,77 @@ import {
 	View,
 } from 'react-native';
 import Box from '../components/Box';
-import { WeatherContext } from '../context/WeatherProvider';
+import { Weather } from '../data';
 import { TabParamList } from '../navigators/TabNavigator';
+
+const API_KEY = 'a3b13214b49043318a485725240210';
 
 type Props = BottomTabScreenProps<TabParamList, 'Home'>;
 
 export default function HomeScreen({ navigation }: Props) {
-	const { weather } = useContext(WeatherContext);
+	const [location, setLocation] = useState<LocationObject>();
+	const [isLoading, setIsLoading] = useState(true);
+	const [weather, setWeather] = useState<Weather>();
+	let batteryLevel = useBatteryLevel();
+	batteryLevel = Math.round(batteryLevel * 100);
 
-	const feelLikeTemp = weather.current.feelslike_c;
-	const temp = weather.current.temp_c;
+	useEffect(() => {
+		(async () => {
+			let { status } = await Location.requestForegroundPermissionsAsync();
+			if (status !== 'granted') {
+				console.log('Permission to access location was denied');
+				return;
+			}
+
+			let location = await Location.getCurrentPositionAsync({});
+			setLocation(location);
+			console.log(
+				`location: lat ${location.coords.latitude}, lon ${location.coords.longitude}`
+			);
+		})();
+	}, []);
+
+	useEffect(() => {
+		async function fetcher() {
+			const lat = location!.coords.latitude;
+			const lon = location!.coords.longitude;
+			const lang = 'sv';
+			const url = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${lat},${lon}&lang=${lang}`;
+
+			const response = await fetch(url);
+			const data: Weather = await response.json();
+			console.log(data);
+			setWeather(data);
+			setIsLoading(false);
+			console.log(`Land: ${weather!.location.country}`);
+			console.log(`namn: ${weather!.location.name}`);
+		}
+		fetcher();
+	}, [location]);
+
+	const temp = weather?.current.temp_c ?? 0;
+	const feelsLikeTemp = weather?.current.feelslike_c ?? 0;
+
 	let tempIcon = 'temperature-low';
 
-	if (feelLikeTemp > temp) {
+	if (feelsLikeTemp > temp) {
 		tempIcon = 'temperature-arrow-up';
 	}
-	if (feelLikeTemp < temp) {
+	if (feelsLikeTemp < temp) {
 		tempIcon = 'temperature-arrow-down';
 	}
 
 	return (
-		<View style={[s.rootContainer, { backgroundColor: '#f28482' }]}>
-			<ActivityIndicator
-				size="large"
-				color="#0000ff"
-			/>
-			<Text>Laddar väderinformation...</Text>
+		<View style={{ backgroundColor: '#f28482', flex: 1 }}>
+			{isLoading && (
+				<View
+					style={{ flex: 1, justifyContent: 'center', alignContent: 'center' }}>
+					<ActivityIndicator
+						size="large"
+						color="#0000ff"
+					/>
+				</View>
+			)}
 			{weather && (
 				<View style={[s.rootContainer, { backgroundColor: '#f28482' }]}>
 					<View
@@ -69,87 +117,52 @@ export default function HomeScreen({ navigation }: Props) {
 									paddingHorizontal: 25,
 								}}>
 								<Box
+									title="Batteri"
+									icon="battery-half"
+									value={batteryLevel}
+									unit="%"
+									onPress={() => navigation.navigate('Details')}
+								/>
+								<Box
 									title="Steg idag"
 									icon="person-walking-arrow-right"
 									value={100}
-									onPress={() => navigation.navigate('Details')}
+									onPress={() => navigation.navigate('Weather')}
 								/>
 								<Box
 									title="Temperatur"
 									icon="temperature-low"
 									value={temp}
 									unit="℃"
-									onPress={() => navigation.navigate('Details')}
+									onPress={() => navigation.navigate('Weather')}
 								/>
 								<Box
 									title="Känns som"
 									icon={tempIcon}
-									value={feelLikeTemp}
+									value={feelsLikeTemp}
 									unit="℃"
-									onPress={() => navigation.navigate('Details')}
+									onPress={() => navigation.navigate('Weather')}
 								/>
 								<Box
-									title="Känns som"
-									icon={tempIcon}
-									value={feelLikeTemp}
-									unit="℃"
-									onPress={() => navigation.navigate('Details')}
+									title="Molnighet"
+									icon="cloud-sun"
+									value={weather.current.cloud}
+									unit="%"
+									onPress={() => navigation.navigate('Weather')}
 								/>
 								<Box
-									title="Känns som"
-									icon={tempIcon}
-									value={feelLikeTemp}
-									unit="℃"
-									onPress={() => navigation.navigate('Details')}
+									title="Vindstyrka"
+									icon="wind"
+									value={weather.current.wind_kph}
+									unit="km/h"
+									onPress={() => navigation.navigate('Weather')}
 								/>
 								<Box
-									title="Känns som"
-									icon={tempIcon}
-									value={feelLikeTemp}
-									unit="℃"
-									onPress={() => navigation.navigate('Details')}
-								/>
-								<Box
-									title="Känns som"
-									icon={tempIcon}
-									value={feelLikeTemp}
-									unit="℃"
-									onPress={() => navigation.navigate('Details')}
-								/>
-								<Box
-									title="Känns som"
-									icon={tempIcon}
-									value={feelLikeTemp}
-									unit="℃"
-									onPress={() => navigation.navigate('Details')}
-								/>
-								<Box
-									title="Känns som"
-									icon={tempIcon}
-									value={feelLikeTemp}
-									unit="℃"
-									onPress={() => navigation.navigate('Details')}
-								/>
-								<Box
-									title="Känns som"
-									icon={tempIcon}
-									value={feelLikeTemp}
-									unit="℃"
-									onPress={() => navigation.navigate('Details')}
-								/>
-								<Box
-									title="Känns som"
-									icon={tempIcon}
-									value={feelLikeTemp}
-									unit="℃"
-									onPress={() => navigation.navigate('Details')}
-								/>
-								<Box
-									title="Känns som"
-									icon={tempIcon}
-									value={feelLikeTemp}
-									unit="℃"
-									onPress={() => navigation.navigate('Details')}
+									title="Sikt"
+									icon="glasses"
+									value={weather.current.vis_km}
+									unit="km"
+									onPress={() => navigation.navigate('Weather')}
 								/>
 							</View>
 						</ScrollView>
